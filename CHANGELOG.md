08f46fd6c6d85a2f40633c7f8d0c27eea6e9d6bf
Justin Love <git@JustinLove.name>
2016-07-02 15:18:50 -0500

    restore fallback icons

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # BaBoom
 
+## 0.7.0
+
+- Restore fallback icons
+
 ## 0.6.0
 
 - Remove Icon Reloader dependency
--- ui/mods/baboom/global.js
+++ ui/mods/baboom/global.js
@@ -1,6 +1,7 @@
 if (window.HodgePodge) {
   HodgePodge.addUnits([{
     spec_id: '/pa/units/land/baboom/baboom.json',
+    si_fallback: ['nuke_launcher_ammo', 'tank_nuke', 'bot_bomb'],
     preferred_builds: [['bot', 1]],
   }])
 }
